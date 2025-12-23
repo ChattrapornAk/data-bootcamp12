05fdabd23e6de0295c1bf86fc238a124f18d35c4
ChattrapornAk <shortstorycreator2024@gmail.com>
2025-12-23 20:53:35 +0700

    print change msg

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-# print hello world
-print("Hello World")
-
-# print goodby
-print("Good Bye")
-
-# new function
-def two_num(num):
-  return num+2
-
-print two_num(2), two_num(5)
+# print final
+print("Now, I know what is Git and GitHub. Easy!")
